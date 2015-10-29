a70c32084be42e6e67e9fa535d9bef39a6f6da1e
Steven Silvester <steven.silvester@ieee.org>
2015-10-29 09:48:45 -0500

    wip

--- example/index.js
+++ example/index.js
@@ -1,14 +1,24 @@
 var lib = require('phosphor-plugins/index');
+var expect = require('expect.js');
 
+console.log('index imported');
 
-lib.fetchPlugins().then(() => {
-    return lib.loadPlugin('foo').then(() => {
-        console.log('foo finished loading');
-        return lib.loadPlugin('bar').then(() => {
-            console.log('bar finished loading');
-            lib.unloadPlugin('bar');
-            lib.unloadPlugin('foo');
-            console.log('all plugins unloaded');
-        });
+describe('example', () => {
+    it('should properly excercise api', (done) => {
+        lib.fetchPlugins().then(() => {
+            expect(lib.listPlugins()).to.eql(['bar', 'foo']);
+            return lib.loadPlugin('foo').then(() => {
+                console.log('foo finished loading');
+                return lib.loadPlugin('bar').then(() => {
+                    console.log('bar finished loading');
+                    lib.unloadPlugin('bar');
+                    lib.unloadPlugin('foo');
+                    console.log('all plugins unloaded');
+                    done();
+                });
+            });   
+        }); 
     });
 });
+
+mocha.run();
